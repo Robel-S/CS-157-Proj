d577a6451fb42d7dae5fbc01f6b8ea80466f78a5
Henry Tran <henryctran02@yahoo.com>
2025-05-07 15:26:02 -0700

    Created Ratings page

--- src/main/java/com/example/cs157proj/model/DatabaseController.java
+++ src/main/java/com/example/cs157proj/model/DatabaseController.java
@@ -4,24 +4,25 @@ import java.sql.*;
 public class DatabaseController {
 
     private Statement statement;
+
     public DatabaseController() {
-        ConnectDB db = new ConnectDB(); //create an object for connecting to our databse
+        ConnectDB db = new ConnectDB(); //create an object for connecting to our database
         Connection connection = db.getConnection(); //gets connection from our ConnectDB Object
         if (connection != null) { //if the database is connected create movie, customer, rental, and rating tables
             try {
-                 statement = connection.createStatement();
+                statement = connection.createStatement();
                 String movieTable = "CREATE TABLE IF NOT EXISTS movie (movieID INTEGER NOT NULL PRIMARY KEY, " +
                         "title VARCHAR(50), genre VARCHAR(50), stock INTEGER, avgRating DECIMAL (1,1))";
-                 String customerTable = "CREATE TABLE IF NOT EXISTS customer (customerID INTEGER NOT NULL PRIMARY KEY, " +
+                String customerTable = "CREATE TABLE IF NOT EXISTS customer (customerID INTEGER NOT NULL PRIMARY KEY, " +
                         "username VARCHAR(50), password VARCHAR(50), name VARCHAR(50), age INTEGER)";
-                 String rentalTable = "CREATE TABLE IF NOT EXISTS rental (customerID INTEGER NOT NULL, " +
+                String rentalTable = "CREATE TABLE IF NOT EXISTS rental (customerID INTEGER NOT NULL, " +
                          "movieID INTEGER NOT NULL, dueDate VARCHAR(50), PRIMARY KEY (customerID, movieID))";
                 String ratingTable = "CREATE TABLE IF NOT EXISTS rating (customerID INTEGER NOT NULL, " +
                         "movieID INTEGER NOT NULL, rating DECIMAL (1,1), PRIMARY KEY (customerID, movieID))";
-                 createTable(movieTable);
-                 createTable(customerTable);
-                 createTable(rentalTable);
-                 createTable(ratingTable);
+                createTable(movieTable);
+                createTable(customerTable);
+                createTable(rentalTable);
+                createTable(ratingTable);
 
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -40,6 +41,10 @@ public class DatabaseController {
 
     }
     public void insertCustomer(String title, String genre, int stock, int avgRating) {
+
+    }
+
+    public void insertRating(int customerID, int movieID, double rating) {
 
     }
 }
